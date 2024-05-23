12d2c287a58949175c9f5b8a3746e1f05efba5c6
brenog4briel <breno-112011@hotmail.com>
2024-05-23 18:18:56 -0300

    feat: layout inicial do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,30 +1,28 @@
 import styles from "./header.module.css"
 import logo from "../../assets/mercearia-logo.png"
-import { AiOutlineSearch } from "react-icons/ai";
-import { InputAdornment, TextField } from "@mui/material";
+import { Link } from "react-router-dom"
+import { BiSearch } from "react-icons/bi"
 
 export default function Header() {
   return (
-      <header className={styles.header}>
-          <img className={styles.logo} src={logo} alt="" />
-          <div className={styles.container_search}>
-              <TextField sx={{width:"100%"}} 
-                InputProps={{endAdornment:(
-                  <InputAdornment position="end">
-                      <AiOutlineSearch/>
-                  </InputAdornment>)}}>
-              </TextField>
-          </div>
-          <nav className={styles.menu_navegacao}>
-              <a href="">Página inicial</a>
-              <a href="">Categorias</a>
-              <a href="">Sobre</a>
-              <a href="">Contato</a>
-          </nav>
-            <div className={styles.container_buttons}>
-              <a className={styles.signin_btn} href="">Entrar</a>
-              <a className={styles.signup_btn} href="">Registre-se</a>
-          </div>
-      </header>
+     <header className={styles.header}>
+      <Link to="">
+         <img src={logo} alt="" className={styles.logo}/>
+      </Link>
+      <div className={styles.search}>
+         <input type="search" name="" id="" placeholder="Digite o nome do produto que deseja buscar"/>
+         <BiSearch size={30} className={styles.icon}/>
+      </div>
+      <nav className={styles.menu_navegacao}>
+         <Link className={styles.link} to=""><p>Página inicial</p></Link>
+         <Link className={styles.link}to=""><p>Categorias</p></Link>
+         <Link className={styles.link} to=""><p>Sobre</p></Link>
+         <Link className={styles.link}to=""><p>Contato</p></Link>
+      </nav>
+      <div>
+         <a href="" className={styles.btn}>Entrar</a>
+         <a href="" className={styles.btn}>Cadastre-se</a>
+      </div>
+     </header>
   )
 }
