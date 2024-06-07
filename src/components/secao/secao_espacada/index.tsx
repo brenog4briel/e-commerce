0cc4f49fcc3f4cadd7411d3f8d440ea8c54eb1c0
Breno Gabriel Da Silva Sacerdote <breno.sacerdote@semam.local>
2024-06-07 09:50:49 -0300

    feat: adicao de keys no map na secao separada

--- src/components/secao/secao_espacada/index.tsx
+++ src/components/secao/secao_espacada/index.tsx
@@ -3,8 +3,8 @@ import styles from "./secao_espacada.module.css"
 export const Secao_Espacada:React.FC<{data:Array<any>}> = ({data}) => {
     return (
         <section className={styles.secao}>
-            {data.map((element) => (
-                <div className={styles.secao_item}>
+            {data.map((element,index) => (
+                <div key={index} className={styles.secao_item}>
                     <img src={element} alt="" />
                 </div>
             ))}
