f036279ee596583ca4e4cdf14091a4c98c491fb9
brenog4briel <breno-112011@hotmail.com>
2024-07-11 17:21:32 -0300

    feat: ajuste do layout da pagina de cadastro de produtos

--- src/pages/cadastroProduto/index.tsx
+++ src/pages/cadastroProduto/index.tsx
@@ -8,7 +8,7 @@ import { useState } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 import { Popup } from "../../components/popup"
-import { CircularProgress } from "@mui/material"
+import { Box, Button, CircularProgress, IconButton, InputAdornment, MenuItem, TextField } from "@mui/material"
 
 interface IRequestError {
   mensagem:string;
@@ -21,17 +21,22 @@ export function CadastroProduto() {
 
     const navigate = useNavigate();
     const [file,setFile] = useState<File>()
-    const [categoria,setCategoria] = useState<string>("tecnologia");
+    const [categoria,setCategoria] = useState<string>("");
 
     const [productRequestError,setProductInfoRequestError] = useState<IRequestError>({mensagem:"",sucesso:false})
     const [productInfoLoading,setProductInfoLoading] = useState<boolean>(false);
     const [productInfoError,setProductInfoError] = useState<boolean | undefined>(undefined);
 
-    const [valor,setValor] = useState<string>("");
+    const [valor,] = useState<string>("");
     const [valorError,setValorError] = useState<boolean>(false)
 
     const [estoque,setEstoque] = useState<string>("");
     const [estoqueError,setEstoqueError] = useState<boolean>(false)
+
+    const [proprietario,setProprietario] = useState<string>("")
+    const [nome,setNome] = useState<string>("")
+    const [preco,setPreco] = useState<string>("")
+
 
     const {register:registerNovoProduto,handleSubmit:handleSubmitNovoProduto,formState:{errors:novoProdutoErrors}} = useForm<NovoProdutoSchema>({
         resolver:zodResolver(novoProdutoSchema)
@@ -123,29 +128,100 @@ export function CadastroProduto() {
                     <input type="file" name="image" onChange={(e) => onChangeImage(e)} />
             </div>
             
-            <div className={styles.product_info}>
-                <form className={styles.info_form} onSubmit={handleSubmitNovoProduto(handleNovoProduto)}>
-                    <input type="text" placeholder="Nome" {...registerNovoProduto("nome")}/>
-                    {novoProdutoErrors.nome && <p className={styles.input_error_message}>{novoProdutoErrors.nome.message}</p>}
-                    <input type="text" placeholder="Preço" value={valor} onChange={(e) => setValor(e.target.value)}/>
-                    {valorError && <p className={styles.input_error_message}>Valor inválido! Ex: 23.50</p>}
-                    <input type="text" placeholder="Proprietário" {...registerNovoProduto("proprietario")}/>
-                    {novoProdutoErrors.proprietario && <p className={styles.input_error_message}>{novoProdutoErrors.proprietario.message}</p>}
-                    <select name="Categoria" value={categoria} onChange={(e) => setCategoria(e.target.value)}>
-                        <option value="tecnologia">Tecnologia</option>
-                        <option value="livros">Livros</option>
-                        <option value="vestimentas">Vestimentas</option>
-                        <option value="alimentacao">Alimentação</option>
-                        <option value="cama_mesa_banho">Cama, mesa e banho</option>
-                        <option value="eletrodomesticos">Eletrodomésticos</option>
-                    </select>
-                    <input type="text" placeholder="Estoque" value={estoque} onChange={(e) => setEstoque(e.target.value)}/>
-                    {estoqueError && <p className={styles.input_error_message}>Valor inválido! Ex: 20</p>}
-                    {!productInfoLoading && <button type="submit">Cadastrar</button>}
-                    {productInfoLoading && <CircularProgress size={25} sx={{alignSelf:"center"}}/>}
+            
+            <Box sx={{display:"flex",flexDirection:"column", justifyItems:"center",alignItems:"center",gap:"20px",padding:5}}>
+               <TextField 
+                    sx={{width:"80%"}}
+                    error={novoProdutoErrors.nome ? true:  false}
+                    helperText={novoProdutoErrors.nome? novoProdutoErrors.nome.message : ""}
+                    size="small"
+                    type="text"
+                    placeholder="Nome" 
+                    {...registerNovoProduto("nome")} 
+                    value={nome} 
+                    onChange={(e) => setNome(e.target.value)} 
+                    InputProps={
+                        {endAdornment: 
+                            <InputAdornment position="end">
+                                <IconButton>
+                                
+                                </IconButton>
+                            </InputAdornment>,
+                        }}/>
 
-                </form>
-            </div>
+                <TextField 
+                    sx={{width:"80%"}}
+                    error={valorError}
+                    helperText={valorError ? "Valor inválido! Ex: 23.50" : ""}
+                    size="small"
+                    type="text"
+                    placeholder="Preço" 
+                    value={preco} 
+                    onChange={(e) => setPreco(e.target.value)} 
+                    InputProps={
+                        {endAdornment: 
+                            <InputAdornment position="end">
+                                <IconButton>
+                                
+                                </IconButton>
+                            </InputAdornment>,
+                        }}/>
+                
+                <TextField 
+                    sx={{width:"80%"}}
+                    error={novoProdutoErrors.proprietario ? true:  false}
+                    helperText={novoProdutoErrors.proprietario? novoProdutoErrors.proprietario.message : ""}
+                    size="small"
+                    type="text"
+                    placeholder="Proprietário" 
+                    {...registerNovoProduto("proprietario")} 
+                    value={proprietario} 
+                    onChange={(e) => setProprietario(e.target.value)} 
+                    InputProps={
+                        {endAdornment: 
+                            <InputAdornment position="end">
+                                <IconButton>
+                                
+                                </IconButton>
+                            </InputAdornment>,
+                        }}/>
+
+                    <TextField
+                        sx={{width:"80%"}}
+                        size="small"
+                        variant="outlined"
+                        select
+                        value={categoria}
+                        label="Categorias"
+                        onChange={(e) => setCategoria(e.target.value)}
+                    >
+                        <MenuItem value="tecnologia">Tecnologia</MenuItem>
+                        <MenuItem value="livros">Livros</MenuItem>
+                        <MenuItem value="vestimentas">Vestimentas</MenuItem>
+                        <MenuItem value="alimentacao">Alimentacao</MenuItem>
+                        <MenuItem value="cama_mesa_banho">Cama, Mesa e banho</MenuItem>
+                        <MenuItem value="eletrodomesticos">Eletrodomesticos</MenuItem>
+                    </TextField>
+
+               <TextField 
+                    sx={{width:"80%"}}
+                    error={estoqueError}
+                    helperText={estoqueError ? "Valor inválido! Ex: 20" : ""}
+                    size="small"
+                    placeholder="Estoque" 
+                    value={estoque} 
+                    onChange={(e) => setEstoque(e.target.value)} 
+                    InputProps={{endAdornment: 
+                        <InputAdornment position="end">
+                            <IconButton>
+                            </IconButton>
+                        </InputAdornment>,
+                }}/>
+
+                {!productInfoLoading && <Button type="submit" variant="contained" sx={{backgroundColor:"green","&:hover":{backgroundColor:"green"}}} name="Entrar" onClick={handleSubmitNovoProduto(handleNovoProduto)}>Entrar</Button>}
+                {productInfoLoading && <CircularProgress size={25} sx={{alignSelf:"center"}}/>}
+        </Box>
+
         </div>
                 {((productInfoError === true) || (productInfoError === false)) && <Popup mensagem={productRequestError.mensagem} sucesso={productRequestError.sucesso}/>}   
     </div>
