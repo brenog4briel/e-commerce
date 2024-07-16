116404a574cd5c0cc9c3a3843e67969bb9f529a5
brenog4briel <breno-112011@hotmail.com>
2024-07-16 12:24:24 -0300

    feat: finalização do projeto

--- src/pages/contato/index.tsx
+++ src/pages/contato/index.tsx
@@ -1,5 +1,16 @@
+import { Box, Button, Stack } from "@mui/material";
+
 export function Contato() {
   return (
-    <h1>Contato</h1>  
+    <Box sx={{display:"flex",flexDirection:"column",alignItems:"center", justifyContent:"space-evenly",padding:10,minHeight:"50vh", textAlign:"flex-start",backgroundColor:"#eeeeee"}}>
+      <h4>Olá, me chamo Breno Gabriel, sou um graduando em Ciência da Computação pela Universidade Federal de Sergipe</h4> <br/>
+      <h4>Sempre entusiasta de games, possivelmente surgiu daí a inspiração para seguir a carreira na área de TI </h4>
+      <Stack direction="row" width='50%' display="flex" justifyContent="space-evenly" alignItems='center'>
+        <Button variant="contained" sx={{backgroundColor:"white",color:"blue","&:hover":{backgroundColor:"white",color:"blue"}}} href="https://www.linkedin.com/in/breno-gabriel-da-silva-sacerdote" target="_blank">Linkedin</Button>
+        <Button variant="contained" sx={{backgroundColor:"white",color:"blue","&:hover":{backgroundColor:"white",color:"blue"}}} href="https://github.com/brenog4briel" target="_blank">Github</Button>
+      </Stack>
+      <h4>Email para contato: brenosacerdotr@gmail.com</h4>
+
+    </Box> 
     )
 }
--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -1,5 +1,20 @@
+import { Box } from "@mui/material";
+
 export function Sobre() {
   return (
-    <h1>Sobre</h1>  
+     <Box sx={{display:"flex",flexDirection:"column",alignItems:"flex-start", padding:10,minHeight:"50vh", textAlign:"flex-start",backgroundColor:"#eeeeee"}}>
+      <h4>Este projeto foi desenvolvido com a finalidade de melhorar minhas habilidades em programação, na qual foi desenvolvido um protótipo de e-commerce</h4> <br/>
+      <h4>Neste projeto o usuário poderá realizar uma compra de forma simulada, cadastrar produtos e navegar através dos produtos cadastrados do seu interesse. 
+        Cada produto foi inserido em uma categoria, o que possibilita o usuário a navegar somente pela sua área de interesse. 
+        Além disso, foi implementado funções secundárias como adição em lista de desejos e visualização do pedido de compra. </h4> <br/>
+
+      <h4>Tecnologias utilizadas:</h4> <br/>
+      <ul>
+        <li>NodeJS</li>
+        <li>Fastify</li>
+        <li>React</li>
+        <li>MySQL</li>
+      </ul>
+    </Box> 
     )
 }
