5babae11fc7631a52ad23e39f37278d0ad39a32f
brenog4briel <breno-112011@hotmail.com>
2024-07-19 12:37:43 -0300

    fix: ajustes na pagina de contato e pagina de sobre

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 
 export function Sobre() {
   return (
-     <Box sx={{display:"flex",flexDirection:"column",alignItems:"flex-start", padding:10,minHeight:"50vh", textAlign:"flex-start",backgroundColor:"#eeeeee"}}>
+     <Box sx={{display:"flex",flexDirection:"column",alignItems:"flex-start", padding:10,minHeight:"100vh", textAlign:"flex-start",backgroundColor:"white"}}>
       <h4>Este projeto foi desenvolvido com a finalidade de melhorar minhas habilidades em programação, na qual foi desenvolvido um protótipo de e-commerce</h4> <br/>
       <h4>Neste projeto o usuário poderá realizar uma compra de forma simulada, cadastrar produtos e navegar através dos produtos cadastrados do seu interesse. 
         Cada produto foi inserido em uma categoria, o que possibilita o usuário a navegar somente pela sua área de interesse. 
